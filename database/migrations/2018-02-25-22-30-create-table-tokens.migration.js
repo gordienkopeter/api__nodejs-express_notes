34450604a63c41fdae73e345a7a6048b6d48b733
Peter_V <gordienkopeter@gmail.com>
2018-02-25 22:44:43 +0200

    add 'tokens' table and model

---
+++ database/migrations/2018-02-25-22-30-create-table-tokens.migration.js
@@ -0,0 +1,29 @@
+/**
+ * This migration describes create tokens table.
+ */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface
+      .createTable('tokens', {
+        uuid: {
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          primaryKey: true
+        },
+        userUuid: {
+          type: Sequelize.UUID,
+          defaultValue: false,
+          allowNull: false
+        },
+        token: {
+          type: Sequelize.STRING,
+          defaultValue: false,
+          allowNull: false
+        },
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE
+      })
+      .then(() => queryInterface.addIndex('tokens', { fields: ['uuid', 'userUuid'] }));
+  },
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('tokens')
+};
---
+++ database/models/token.model.js
@@ -0,0 +1,30 @@
+const DB = require('../');
+const Sequelize = require('sequelize');
+
+/**
+ * This model describes tokens table properties.
+ */
+const TokenModel = DB.instance.define(
+  'tokens',
+  {
+    uuid: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true
+    },
+    userUuid: {
+      type: Sequelize.UUID,
+      notNull: true
+    },
+    token: Sequelize.STRING,
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+  },
+  {
+    timestamps: true
+  }
+);
+
+TokenModel.removeAttribute('id');
+
+module.exports = TokenModel;
