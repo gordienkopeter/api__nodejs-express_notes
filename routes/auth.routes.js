6c771dcf83a74efe877f6cecf402b46d655cd273
Peter_V <gordienkopeter@gmail.com>
2018-02-25 22:21:32 +0200

    add verify endpoint

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,17 +3,25 @@ const RegisterController = require('../controllers/register.controller');
 const LoginController = require('../controllers/login.controller');
 const RegisterMiddleware = require('../middlewares/register.middleware');
 const LoginMiddleware = require('../middlewares/login.middleware');
+const AuthenticateMiddleware = require('../middlewares/authenticate.middleware');
 
 /**
  * Auth endpoint middleware list.
  */
 authRouter.post('/register', (req, res, next) => RegisterMiddleware(req, res, next));
 authRouter.post('/login', (req, res, next) => LoginMiddleware(req, res, next));
+authRouter.get('/verify', AuthenticateMiddleware);
 
 /**
  * Auth endpoint list.
  */
 authRouter.post('/register', (req, res) => RegisterController.register(req, res));
 authRouter.post('/login', (req, res) => LoginController.login(req, res));
+authRouter.get('/verify', (req, res) =>
+  res.json({
+    status: 'ok',
+    message: 'Your token verified successfully.'
+  })
+);
 
 module.exports = authRouter;
